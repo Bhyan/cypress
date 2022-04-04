623d6872df0db1135c8f6c3c3772de4717e16f02
Bryan Brito <bhyanbrito@gmail.com>
2022-04-04 09:12:14 -0300

    Validação de opacity nos elementos

--- cypress/support/pages/resetpass/elements.js
+++ cypress/support/pages/resetpass/elements.js
@@ -1,4 +1,5 @@
 exports.el = {
+    title: ('h1', 'Resetar senha'),
     password: 'input[placeholder="Nova senha"]',
     passConfirm: 'input[placeholder="Confirmação da senha"]',
     changeButton: ('button[type=submit]', 'Alterar senha')
